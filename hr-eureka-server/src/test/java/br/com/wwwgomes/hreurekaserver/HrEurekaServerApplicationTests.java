e7e1ce9f87579328b94ac5b844fb5fdabf7ec5b7
Williams Gomes <williamsgomess@outlook.com>
2021-02-27 21:02:27 -0300

    Project hr-eureka-server created

---
+++ hr-eureka-server/src/test/java/br/com/wwwgomes/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wwwgomes.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
