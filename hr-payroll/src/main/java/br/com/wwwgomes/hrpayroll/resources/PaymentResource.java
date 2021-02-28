febea50e054e0fa14088b8c658d5dacf5699bcc3
Williams Gomes <williamsgomess@outlook.com>
2021-02-27 23:41:03 -0300

    Api Gateway
    Create hr-api-gateway-zuul
    Configuring Zuul Api Gateway

--- hr-payroll/src/main/java/br/com/wwwgomes/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/br/com/wwwgomes/hrpayroll/resources/PaymentResource.java
@@ -2,6 +2,7 @@ package br.com.wwwgomes.hrpayroll.resources;
 
 import br.com.wwwgomes.hrpayroll.entities.Payment;
 import br.com.wwwgomes.hrpayroll.service.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,9 +21,15 @@ public class PaymentResource {
         this.paymentService = paymentService;
     }
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping("/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
         var payment = paymentService.getPayment(workerId, days);
+        return ResponseEntity.ok(payment);
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) {
+        var payment = new Payment("Brian", 400.0, days);
         return ResponseEntity.ok(payment);
     }
 }
